aeadf3fa821aeb54b26a542ea84c4ef7360aced2
Clinscott <clinscott@gmail.com>
2022-04-16 16:16:50 -0400

    Video Player Resizes
    some css changes to make the video player response to screen size.

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -3,11 +3,13 @@ import utilStyles from "../styles/utils.module.css"
 
 export default function VideoPlayer(props){
     return (
-        <div className={utilStyles.video}>
+        <div className={utilStyles.videoWrapper}>
             <h4>{props.title}</h4>
             <ReactPlayer
+                className={utilStyles.reactPlayer}
+                width='100%'
                 url={props.src}
                 />
-        </div>
+            </div>
     )
 }
