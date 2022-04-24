1f19fba39e3298ee90c0e1e5525f5d8cd748dc15
Clinscott <clinscott@gmail.com>
2022-04-24 16:07:22 -0400

    Founder Images to Index

--- components/layout.js
+++ components/layout.js
@@ -36,6 +36,7 @@ export default function Layout({children, home}){
                             width={profilePic.width}
                             alt={name}
                         />
+                        
                     </>
                 ) : (
                     <>
--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,40 @@ const profilePic = {
   height: 32
 }
 
+const founderImage = {
+  width: 128,
+  height: 128
+}
+
 export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <div className={utilStyles.rowC2}>
+      <Image
+        priority
+        src='/images/NH-CraigFull.gif'
+        className={utilStyles.borderCircle}
+        height={founderImage.height}
+        width={founderImage.width}
+        alt='Craig Linscott'
+        styles={utilStyles.image}
+        />
       <section className={utilStyles.headingMd}>
         <h6>We are Northammer! Together we look to provide exciting, engaging and informative Warhammer content.</h6>
       </section>
+      <Image
+        priority
+        src='/images/NH-BradFull.gif'
+        className={utilStyles.borderCircle}
+        height={founderImage.height}
+        width={founderImage.width}
+        alt='Brad Linscott'
+        styles={utilStyles.image}
+        />
+      </div>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.blogArticle}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
