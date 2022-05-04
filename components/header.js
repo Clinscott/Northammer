ac020f520a065ea238d7234347057b15bee78b0f
Clinscott <clinscott@gmail.com>
2022-05-04 10:44:58 -0400

    Header
    A bit cleaner transition.

--- components/header.js
+++ components/header.js
@@ -1,82 +1,81 @@
-import {useEffect, useState} from 'react';
-import Link from 'next/link';
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Image from 'next/image';
-import Button from './button';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
+import Button from "./button";
 
-
-const name = 'Northammer';
+const name = "Northammer";
 const profilePicStatic = {
-    width: 750,
-    height: 275
-}
-const profilePicOnScroll= {
-    width: 125,
-    height: 50
-}
-let profilePic = profilePicStatic
+  width: 750,
+  height: 275,
+};
+const profilePicOnScroll = {
+  width: 125,
+  height: 50,
+};
+let profilePic = profilePicStatic;
 
+export default function Header(props) {
+  const [scrolling, setScrolling] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
 
-export default function Header(props){
-    const [scrolling, setScrolling] = useState(false);
-    const [scrollTop, setScrollTop] = useState(0);
-  
-    useEffect(() => {
-      function onScroll() {
-        let currentPosition = window.pageYOffset; // or use document.documentElement.scrollTop;
-        if (currentPosition > 200) {
-          console.log('Scrolling Down')
-          profilePic = profilePicOnScroll
-          setScrolling(false);
-        } else {
-          console.log('Scrolling Up')
-          setScrolling(true);
-        }
-        if(currentPosition === 0){
-            profilePic = profilePicStatic
-        }
-        setScrollTop(currentPosition <= 0 ? 0 : currentPosition);
+  useEffect(() => {
+    function onScroll() {
+      let currentPosition = window.pageYOffset; // or use document.documentElement.scrollTop;
+      if (currentPosition > 200) {
+        console.log("Scrolling Down");
+        profilePic = profilePicOnScroll;
+        setScrolling(false);
+      } else {
+        console.log("Scrolling Up");
+        setScrolling(true);
       }
-  
-      window.addEventListener("scroll", onScroll);
-      return () => window.removeEventListener("scroll", onScroll);
-    }, [scrollTop]);
-  
-    return(
-<header className={styles.header}>
-                {props.home ? (
-                    <>
-                        <Image 
-                            priority
-                            src="/images/profile.png"
-                            height={profilePic.height}
-                            width={profilePic.width}
-                            alt={name}
-                        />
-                        
-                    </>
-                ) : (
-                    <>
-                        <Link href="/">
-                            <a>
-                                <Image
-                                    priority
-                                    src="/images/profile.png"
-                                    height={profilePic.height}
-                                    width={profilePic.width}
-                                    alt={name}
-                                    />
-                            </a>
-                        </Link>
-                        
-                    </>
-                )}
-                <div className= {styles.line}>
-                    <Button title='40k' link="/posts/40k"></Button>
-                    <Button title='Fantasy' link="/posts/fantasy"></Button>
-                    {/*<Button title='Cool Gentleman' link="/posts/coolGentleman"></Button>*/}
-                    <Button title='The Founders' link="/posts/theFounders"></Button>
-                </div>
-                </header>
-    )}
+      if (currentPosition === 0) {
+        profilePic = profilePicStatic;
+      }
+      setScrollTop(currentPosition <= 0 ? 0 : currentPosition);
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
+  return (
+    <header>
+      <div className={styles.header}>
+        {props.home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.png"
+              height={profilePic.height}
+              width={profilePic.width}
+              alt={name}
+            />
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.png"
+                  height={profilePic.height}
+                  width={profilePic.width}
+                  alt={name}
+                />
+              </a>
+            </Link>
+          </>
+        )}
+      </div>
+      <div className={styles.line}>
+        <Button title="40k" link="/posts/40k"></Button>
+        <Button title="Fantasy" link="/posts/fantasy"></Button>
+        {/*<Button title='Cool Gentleman' link="/posts/coolGentleman"></Button>*/}
+        <Button title="The Founders" link="/posts/theFounders"></Button>
+      </div>
+    </header>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,44 +1,38 @@
-import React, {useRef, useEffect} from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Button from './button';
-import Header from './header';
-export const siteTitle = 'Northammer - Home';
+import React, { useRef, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Button from "./button";
+import Header from "./header";
+export const siteTitle = "Northammer - Home";
 
-
-
-export default function Layout({children, home}){
-
-    return(
-        <div>
-            <Head>
-                <link rel='icon' href="/northammerIcon.ico" />
-                <meta 
-                    name='description'
-                    content='The place for engaging and informative Warhammer content.'
-                    />
-                <meta property='og:title' content={siteTitle}/>
-                <meta name='twitter:card' content='summary_large_image'/>
-            </Head>
-            <div className={styles.fixed}>
-            
-                    <Header 
-                        home={home}
-                        />
-            </div>
-            <div className={styles.container}>
-                <main className={styles.main}>{children}</main>
-                    {!home && (
-                        <div className={styles.backToHome}>
-                            <Link href="/">
-                                <a>← Back to home</a>
-                            </Link>
-                        </div>
-                )}
-                </div>
-        </div>
-    )
+export default function Layout({ children, home }) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/northammerIcon.ico" />
+        <meta
+          name="description"
+          content="The place for engaging and informative Warhammer content."
+        />
+        <meta property="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div>
+        <Header home={home} />
+      </div>
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
